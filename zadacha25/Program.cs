acf260aebdf106ed38ed528fac9214f3e26f3613
OlgaT <oliunea-t2002@mail.ru>
2022-08-26 18:40:53 +0300

    add zadacha25

---
+++ zadacha25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+// Например: 3,5-243; 2,4-16.
+var numberA = int.Parse(Console.ReadLine()!);
+var numberB = int.Parse(Console.ReadLine()!);
+
+if(numberB<=0)
+return;
+
+var result = Math.Pow(numberA,numberB);
+Console.WriteLine(result);
+
