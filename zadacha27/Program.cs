afa15903d4d03b6332cb3ece06db12bae67e3dfa
OlgaT <oliunea-t2002@mail.ru>
2022-08-26 18:48:20 +0300

    add zadacha27

---
+++ zadacha27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+// Например: 452-11; 82-10; 9012-12.
+var number = int.Parse(Console.ReadLine()!);
+
+if (number<=0)
+return;
+
+var result = SumNum(number);
+Console.WriteLine(result);
+
+int SumNum(int a)
+{
+    int result = 0;
+    while(a>0)
+    {
+        result=result+a%10;
+        a=a/10;
+    }
+    return result;
+}
